61446a2134ca93e1861f323ce2ad27d31376aa11
vaibhav2codes <vaibhav2112001@gmail.com>
2024-02-13 23:37:56 +0530

    part 5

--- frontend/src/pages/home/HomePage.js
+++ frontend/src/pages/home/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useReducer } from 'react'
-import { getAll } from '../../services/foodService';
+import { getAll, search } from '../../services/foodService';
 import Thumbnails from '../../Components/Thumbnails/Thumbnails';
+import { useParams } from 'react-router-dom';
+import Search from '../../Components/Search/Search';
 
 const initialState = { foods: [] };
 
@@ -18,13 +20,16 @@ const reducer = (state, action) => {
 export default function HomePage() {
     const [state, dispatch] = useReducer(reducer, initialState);
     const {foods} = state;
-    // const { searchTerm, tag } = useParams();
+    const { searchTerm } = useParams();
 
     useEffect(() => {
-        getAll().then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }));
-      },[]);
+      const loadFoods =searchTerm? search(searchTerm): getAll();
+
+        loadFoods.then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }));
+      },[searchTerm]);
   return (
     <>
+      <Search />
       <Thumbnails foods={foods} />
     </>
   )
