02242eee55f7f93f2681d8be9730528759740a79
vaibhav2codes <vaibhav2112001@gmail.com>
2024-02-14 00:27:38 +0530

    Part 6

--- frontend/src/services/foodService.js
+++ frontend/src/services/foodService.js
@@ -1,6 +1,18 @@
-import {sample_foods} from '../data';
+import {sample_foods, sample_tags} from '../data';
 
 export const getAll =async () => sample_foods;
 
 export const search = async searchTerm => 
     sample_foods.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+export const getAllTags =async () => sample_tags;
+
+export const getAllByTag = async (tag) => {
+    const allFoods = await getAll();
+
+    if (tag === 'All') {
+        return allFoods;
+    }
+
+    return allFoods.filter(item => item.tags?.includes(tag));
+};
